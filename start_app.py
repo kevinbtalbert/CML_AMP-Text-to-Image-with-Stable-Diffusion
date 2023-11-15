dfd5ec9aa40732f6a7fb9cf780f478b185e0173e
Kevin Talbert <ktalbert@cloudera.com>
2023-11-15 12:08:21 -0500

    Updated torch requirements

--- start_app.py
+++ start_app.py
@@ -1,3 +1,4 @@
 import subprocess
+import os
 
-print(subprocess.run(["python launch.py --port=${CDSW_APP_PORT}"], shell=True))
+print(subprocess.run(["python launch.py --port " + str(os.environ['CDSW_APP_PORT'])], shell=True))
